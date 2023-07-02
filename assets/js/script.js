b07894034a26f03f8c8ef8a1bd30c9a36e27220e
Yurigabriel6 <131935841+Yurigabriel6@users.noreply.github.com>
2023-07-02 00:10:21 -0300

    Sobre Mim

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-console.warn('Não Mexa Aqui Por Gentileza!!')
-console.warn('Se Você Sabe Oque Está Fazendo Aqui, Por Favor Entre Em Contato Comigo')
-console.warn('Esse Local é Usado Pelo Site Para Mostrar Os Eventos JS Rodando')
-console.error(" Esses Eventos Ainda Não Foram Adicionados")
+console.warn('Não Mexa Aqui Por Gentileza!!');
+console.warn('Se Você Sabe Oque Está Fazendo Aqui, Por Favor Entre Em Contato Comigo');
+console.warn('Esse Local é Usado Pelo Site Para Mostrar Os Eventos JS Rodando');
+console.error(" Esses Eventos Ainda Não Foram Adicionados");
