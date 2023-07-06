2c67afc7042906dcb81dc13cbdffc14e8603fb10
Yurigabriel6 <131935841+Yurigabriel6@users.noreply.github.com>
2023-07-05 21:35:43 -0300

    Manutenção Em Andamento

---
+++ assets/js/manutenção.js
@@ -0,0 +1,15 @@
+document.querySelector('.github').addEventListener('click', function() {
+    open(URL = "https://github.com/Yurigabriel6", target = "_blank");
+});
+
+document.querySelector('.discord').addEventListener('click', function() {
+    open(URL = "https://discord.gg/zvqwF6C5gD", target = "_blank");
+});
+
+insta = document.querySelector('.insta').addEventListener('click', function() {
+    open(URL = "https://www.instagram.com/atomic_yurigabriel6/", target = "_blank");
+});
+
+document.querySelector('.face').addEventListener('click', function() {
+    open(URL = "https://www.facebook.com/profile.php?id=100076595186638", target = "_blank");
+});
